4415c993e9469315d1290f7a62d93d23b4e2d045
njenekar <nitin.jenekar@gmail.com>
2019-08-18 19:58:23 +0530

    Implemented Strategy Pattern example

--- DesignPatternLearnings/Behavioral Pattern/StrategyPattern.cs
+++ DesignPatternLearnings/Behavioral Pattern/StrategyPattern.cs
@@ -1,4 +1,6 @@
-﻿namespace DesignPatternLearnings
+﻿using System;
+
+namespace DesignPatternLearnings
 {
     internal class StrategyPattern
     {
@@ -6,7 +8,48 @@
         {
             public static void Start()
             {
+                Context context;
+                context = new Context(new StrategyA());
+                context.ContextAlgo();
 
+                context = new Context(new StrategyB());
+                context.ContextAlgo();
+            }
+        }
+
+        public class Context
+        {
+            private IStrategy _strategy;
+            public Context(IStrategy strategy)
+            {
+                _strategy = strategy;
+            }
+
+            public void ContextAlgo()
+            {
+                _strategy.DoAlogorithm();
+            }
+        }
+
+        public interface IStrategy
+        {
+            void DoAlogorithm();
+        }
+
+        public class StrategyA : IStrategy
+        {
+            public void DoAlogorithm()
+            {
+                Console.WriteLine("Called Strategy A Algorithm");
+
+            }
+        }
+
+        public class StrategyB : IStrategy
+        {
+            public void DoAlogorithm()
+            {
+                Console.WriteLine("Called Strategy B Algorithm");
             }
         }
     }
